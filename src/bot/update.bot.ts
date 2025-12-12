4dd47e6a387cb324a7aa2a12af49b6ba31f48999
unknown <mjamoliddin2006@gamil.com>
2025-12-12 17:07:43 +0500

    Yangilandi

--- src/bot/update.bot.ts
+++ src/bot/update.bot.ts
@@ -1,16 +1,56 @@
-import { On, Start, Update } from "nestjs-telegraf";
+import { Hears, On, Start, Update, Ctx, Action } from "nestjs-telegraf";
+import { Context } from "telegraf";
 
 
 @Update()
 export class BotUpdate {
   @Start()
   onStart(ctx: any) {
-    ctx.reply(`Welcome to the bot! ${ctx.message!.from.first_name}`);
+    ctx.reply(`Hush kelibsiz ${ctx.message!.from.first_name}!`, {
+      reply_markup: {
+        keyboard: [
+          [{ text: "Info" }, { text: "Profile" }],
+          [{ text: "Help" }]
+        ],
+        resize_keyboard: true,
+      }
+    });
     return;
   }
 
-  @On("text")
-  onText(ctx: any) {
-    ctx.telegraf.sendMessage(ctx.message!.from.id);
+  // @On("text")
+  // onText(ctx: any) {
+  //   ctx.telegraf.sendMessage(ctx.message!.from.id);
+  //   return;
+  // }
+
+  @Hears("Info")
+  getInfo(@Ctx() ctx: Context) {
+    return ctx.reply("This is some information about the bot.", {
+      reply_markup: {
+        inline_keyboard: [
+          [
+            {
+              text: "Shaxsiy",
+              callback_data: "personal_info",
+            },
+          ],
+          [
+            {
+              text: "Texnik",
+              callback_data: "technical_info",
+            },
+          ],
+
+        ]
+      }
+    })
+    return
+  }
+
+  @Action("shaxsiy")
+  getprivate(@Ctx() ctx: Context) {
+   ctx.reply("This is your personal information.");
+    return
   }
 }
